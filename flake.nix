ce06d9e07ca020f5c95ef711c1624b84d7a95ddd
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-08-12 11:49:39 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "k8s-tutorial";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            minikube
+            kubectl
+          ];
+          postShellHook = ''
+          '';
+        };
+      }
+    );
+}
